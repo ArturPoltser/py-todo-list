1613ece54f6e6105fcaf78f9cb323a4153f07f89
Artur Poltser <polcerartur@gmail.com>
2024-03-11 12:56:25 +0200

    add CRUD urls and views for Tag model

--- todo_list/views.py
+++ todo_list/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from todo_list.forms import TaskForm
-from todo_list.models import Task
+from todo_list.models import Task, Tag
 
 
 class IndexView(generic.ListView):
@@ -26,3 +26,25 @@ class TaskUpdateView(generic.UpdateView):
 class TaskDeleteView(generic.DeleteView):
     model = Task
     success_url = reverse_lazy("todo_list:index")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 5
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo_list:tag-list")
