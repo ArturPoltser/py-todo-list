822d322db5575d7458f450a19d5c7246e1031294
Artur Poltser <polcerartur@gmail.com>
2024-03-11 12:33:34 +0200

    add CRUD urls and views for Task model + add form for Create/Update

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from todo_list.views import IndexView
+from todo_list.views import (
+    IndexView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+)
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "todo_list"
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,28 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
+from todo_list.forms import TaskForm
 from todo_list.models import Task
 
 
 class IndexView(generic.ListView):
     model = Task
     template_name = "todo_list/index.html"
+    paginate_by = 5
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_list:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_list:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo_list:index")
